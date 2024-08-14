f5dd93be886540ecd6e02a9c801e254123f54a42
Nassitch <Nassitch@hotmail.fr>
2024-08-14 13:02:21 +0200

    Finish to display all informations.

---
+++ src/app/modules/card/shared/pipes/type-card-color.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'typeCardColor'
+})
+export class TypeCardColorPipe implements PipeTransform {
+
+  transform(type: string): string {
+
+    let cardColor: string;
+
+    switch(type) {
+      case 'fire':
+        cardColor = 'fire_card.png';
+        break;
+      case 'water':
+        cardColor = 'water_card.png';
+        break;
+      case 'grass':
+        cardColor = 'grass_card.png';
+        break;
+      case 'electric':
+        cardColor = 'lightning_card.png';
+        break;
+      case 'fighting':
+        cardColor = 'fighting_card.png';
+        break;
+        default:
+          cardColor = 'dragon_card.png';
+          break;
+    }
+
+    return '../../assets/cards/' + cardColor;
+  }
+
+}
