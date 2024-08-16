1bbcadacea75e58312a07eb432e4ea05b8ef1ccc
David Duranty <dadaduranty@gmail.com>
2024-08-16 09:05:51 +0200

    add button goback home

--- src/app/modules/details/components/details/details.component.ts
+++ src/app/modules/details/components/details/details.component.ts
@@ -2,7 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { DetailsService } from '../../shared/services/details.service';
 import { Observable } from 'rxjs';
 import { PokemonType } from 'src/app/modules/card/models/pokemon.type';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-details',
@@ -12,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent implements OnInit {
   private detailsService = inject(DetailsService);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   pokemon$!: Observable<PokemonType>;
 
@@ -20,5 +22,9 @@ export class DetailsComponent implements OnInit {
     const id: number = Number(this.route.snapshot.paramMap.get("id"));
 
     this.pokemon$ = this.detailsService.getDetailPokemonById$(id);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/'])
   }
 }
