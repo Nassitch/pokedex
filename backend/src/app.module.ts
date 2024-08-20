698b29e00dcb163b8542df3d80fb6d65b4def2de
David Duranty <dadaduranty@gmail.com>
2024-08-20 15:27:02 +0200

    Backend: add auth bcrypt and jwt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 
