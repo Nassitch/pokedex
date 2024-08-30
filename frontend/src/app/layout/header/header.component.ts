b04969ac709edaeee270ebf5e0625a3b27719b09
David Duranty <dadaduranty@gmail.com>
2024-08-30 10:36:08 +0200

    Frontend: add token + login

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/modules/auth/shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent  {
+
+
 
 }
