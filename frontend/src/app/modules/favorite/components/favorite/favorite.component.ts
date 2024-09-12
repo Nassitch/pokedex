43981b62447768a6a129957c7263f98e8e31de85
Nassitch <Nassitch@hotmail.fr>
2024-09-12 17:45:37 +0200

    Front config: -Add 'title' and 'meta' data for each pages.

--- frontend/src/app/modules/favorite/components/favorite/favorite.component.ts
+++ frontend/src/app/modules/favorite/components/favorite/favorite.component.ts
@@ -5,6 +5,7 @@ import { PokemonType } from 'src/app/modules/card/models/pokemon.type';
 import { CardService } from 'src/app/modules/card/shared/services/card.service';
 import { Router } from '@angular/router';
 import { ToastService } from 'src/app/modules/toast/shared/services/toast.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-favorite',
@@ -18,12 +19,18 @@ export class FavoriteComponent implements OnInit {
   protected router = inject(Router);
   private favoriteService = inject(FavoriteService);
   private toastService = inject(ToastService);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   deleteSubscription$: Subscription = new Subscription();
 
   favoriteList$!: Observable<PokemonType[]>;
 
   ngOnInit(): void {
+    this.title.setTitle('Your favorites.');
+    this.meta.updateTag({ name: 'description', content: 'View and delete all of favorite pokemon cards.' });
+    this.meta.updateTag({ name: 'keywords', content: 'View, edit, delete, favorite cards, data, pokemon cards' });
+
     this.favoriteList$ = this.refreshFavoriteList$.pipe(
       switchMap(() => this.favoriteService.getAllFavorite$()),
     );
--- frontend/src/app/modules/search/components/search/search.component.ts
+++ frontend/src/app/modules/search/components/search/search.component.ts
@@ -1,29 +1,32 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { SearchService } from '../../shared/services/search.service';
-import {
-  BehaviorSubject,
-  debounceTime,
-  Observable,
-  Subscription,
-  tap,
-} from 'rxjs';
+import { BehaviorSubject, debounceTime, Observable, Subscription, tap } from 'rxjs';
 import { PokemonType } from 'src/app/modules/card/models/pokemon.type';
 import { Router } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnDestroy {
+export class SearchComponent implements OnInit, OnDestroy {
   private searchService = inject(SearchService);
   private router = inject(Router);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   searchTerms = new BehaviorSubject<PokemonType | null>(null);
 
   pokemonCardList$!: Observable<PokemonType[]>;
 
   private pokemonSub: Subscription = new Subscription();
+  
+  ngOnInit(): void {
+    this.title.setTitle('Seach');
+    this.meta.updateTag({ name: 'description', content: 'Search your favorite pokemons into Pokedex.' });
+    this.meta.updateTag({ name: 'keywords', content: 'Pokedex, search, Pokemons, details, pokemon card.' });
+  }
 
   searchPokemon(name: string): Observable<PokemonType> {
     return this.searchService.getPokemonByName$(name.toLowerCase()).pipe(
