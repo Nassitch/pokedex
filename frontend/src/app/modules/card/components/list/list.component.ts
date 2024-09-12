3caeaa76b4a72579532b14099b43280f6477f28a
Nassitch <Nassitch@hotmail.fr>
2024-09-12 17:58:27 +0200

    Front fix: -Add title and meta for landing component.

--- frontend/src/app/modules/card/components/list/list.component.ts
+++ frontend/src/app/modules/card/components/list/list.component.ts
@@ -3,6 +3,7 @@ import { CardService } from '../../shared/services/card.service';
 import { PokemonType } from '../../models/pokemon.type';
 import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-list',
@@ -12,12 +13,18 @@ import { Router } from '@angular/router';
 export class ListComponent implements OnInit, OnDestroy {
   protected cardService = inject(CardService);
   protected router = inject(Router);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   pokemonList$!: Observable<PokemonType[] | null>;
 
   private pokemonSub: Subscription = new Subscription();
 
   ngOnInit() {
+    this.title.setTitle('Pokedex - by David & Nassime.');
+    this.meta.updateTag({ name: 'description', content: `Navigate and view all pokemon cards.` });
+    this.meta.updateTag({ name: 'keywords', content: `View, pokemons, pokemon cards, data` });
+
     this.pokemonList$ = this.cardService._pokemonDetails$;
 
     this.pokemonSub = this.cardService.getPokemonList$().subscribe();
